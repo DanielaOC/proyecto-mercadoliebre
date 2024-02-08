22c443df12cac3308c467a312223fa5ea6f71e22
DanielaOC <cdortizcil@gmail.com>
2024-02-08 17:23:35 +0100

    realizando cambios

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { register } = require('module');
 const path = require('path');
 
 const app = express();
@@ -13,3 +14,17 @@ app.listen( PORT, () => console.log(`Server up on PORT:  http://localhost:${PORT
 app.get('/', (req, res) => {
  res.sendFile(path.resolve(__dirname, './views/home.html'));
 })
+
+
+app.get('/register', (req, res) => {
+    const pathRegister= path.join(__dirname, './views/register.html')
+    res.sendFile(pathRegister);
+   })
+
+   app.get('/login', (req, res) => {
+    const pathLogin= path.join(__dirname, './views/login.html')
+    res.sendFile(pathLogin);
+   })
+
+
+  
